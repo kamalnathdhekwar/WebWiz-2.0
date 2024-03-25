d6334bce71458d3a5a9511af87138f31835cd2fe
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-26 00:55:54 +0530

    initialized the project with navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import Image from "next/image";
+import Link from 'next/link'
+import navlogo from '../../public/navlogo.svg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className=' text-white items-center flex bg-slate-700 p-5 justify-evenly'>
+        <nav className="b flex  w-full justify-around">
+          <div className='flex gap-9 text-xl'>
+          <Link href="/">
+          <p className='cursor-pointer'>Home</p>
+          </Link>
+          <Link href="/about">
+            <p className='cursor-pointer'>About</p>
+          </Link>
+          <Link href="/contact-us">
+            <p className='cursor-pointer'>Contact Us</p>
+          </Link>
+          <Link href="/support">
+            <p className='cursor-pointer'>Support</p>
+          </Link>
+          </div>
+          
+          <div className='flex  justify-between  '>
+            <button className='cursor-pointer bg-green-700 px-9 py-[6px] rounded-lg'>
+            <Link href="/sign-up">
+            <p className='cursor-pointer'>Sign Up</p>
+           </Link>
+            </button>
+
+            <div>
+            <Image src={navlogo} className='cursor-pointer'/>
+            </div>
+
+          </div>
+
+          
+        </nav>
+    </div>
   )
 }
 
